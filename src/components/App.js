3cc96e9d1626f1b689f6dee700a8b98539c53021
mayank gupta <mayankguptamg462@gmail.com>
2022-11-11 12:53:14 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,9 @@
 import React, { Component, useState } from "react";
 import "./../styles/App.css";
+import NestedDropdown from './NestedDropdown';
+
+
+
 
 // Do not alter the states const and values inside it.
 const states = [
@@ -155,7 +159,9 @@ const states = [
 ];
 
 function App() {
-  return <div id="main"></div>;
+  return <div id="main">
+    <NestedDropdown states={states} />
+  </div>;
 }
 
 export default App;
